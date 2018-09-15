038019a02989cb29c6dd48d57924ffac074c259c
junxi.wjd <junxi.wjd@cainiao.com>
2018-09-15 22:44:01 +0800

    modify nuclear

--- nuclear/nuclear.py
+++ nuclear/nuclear.py
@@ -12,30 +12,30 @@ from spider import SpiderHTML
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-input_path = './books'
-output = './book/nuclear.txt'
+output = '~/Downloads/nuclear_oztf.txt'
 
+help_url = 'http://1024.hlork9.pw/pw/thread.php?fid=17&page=1'
+target_url = 'http://1024.hlork9.pw/pw/htm_data/17/1809/1285939.html'
 
 class NuclearSpider(SpiderHTML):
 
-    def __init__(self, pageStart, pageEnd):
-        self._pageStart = pageStart
-        self._pageEnd = pageEnd
+    def __init__(self, pageStart, pageEnd, url):
+        self._url = url
+        self._pageStart = int(pageStart)
+        self._pageEnd = int(pageEnd) + 1
 
     def start(self):
-        for page in range(self._pageStart, self._pageEnd + 1):
-            filename = '12778p%s.html' % page
-            logger.info('Loading file: %s' % filename)
-            path = os.path.join(input_path, filename)
-            soup = self.loadFromFile(path, coding='gbk')
-            [script.extract() for script in soup('script')]
-            title = soup.h1.string
-            content = soup.body.find('div', class_='mcc').get_text()
-            result = '标题：' + title + '\n' + '内容：' + content + '\n\n'
+        for page in range(self._pageStart, self._pageEnd):
+            url = self._url + str(page) + '.html'
+            logger.info('url is %s' % url)
+            soup = self.getUrl(url)
+            title = soup.body.find('span', id='subject_tpc').get_text()
+            content = soup.body.find('div', id='read_tpc').get_text()
+            result = '标题：' + title + '\n' + '正文：' + content + '\n\n'
             self.saveText(output, result, mode='a')
             logger.info('part %s done.' % page)
 
 
 if __name__ == '__main__':
-    nuclear = NuclearSpider(1, 152)
+    nuclear = NuclearSpider(1273782, 1285939, 'http://1024.hlork9.pw/pw/htm_data/17/1809/')
     nuclear.start()
