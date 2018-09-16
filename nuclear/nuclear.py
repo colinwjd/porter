7143c81d9b0807caf7483ac3f7f033f97cf0b04d
junxi.wjd <junxi.wjd@cainiao.com>
2018-09-16 11:07:50 +0800

    add try except

--- nuclear/nuclear.py
+++ nuclear/nuclear.py
@@ -12,7 +12,7 @@ from spider import SpiderHTML
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-output = '~/Downloads/nuclear_oztf.txt'
+output = './data/nuclear_oztf.txt'
 
 help_url = 'http://1024.hlork9.pw/pw/thread.php?fid=17&page=1'
 target_url = 'http://1024.hlork9.pw/pw/htm_data/17/1809/1285939.html'
@@ -28,14 +28,18 @@ class NuclearSpider(SpiderHTML):
         for page in range(self._pageStart, self._pageEnd):
             url = self._url + str(page) + '.html'
             logger.info('url is %s' % url)
-            soup = self.getUrl(url)
-            title = soup.body.find('span', id='subject_tpc').get_text()
-            content = soup.body.find('div', id='read_tpc').get_text()
-            result = '标题：' + title + '\n' + '正文：' + content + '\n\n'
-            self.saveText(output, result, mode='a')
-            logger.info('part %s done.' % page)
+            try:
+                soup = self.getUrl(url)
+                title = soup.body.find('span', id='subject_tpc').get_text()
+                content = soup.body.find('div', id='read_tpc').get_text()
+                result = '标题：' + title + '\n' + '正文：' + content + '\n\n'
+                self.saveText(output, result, mode='a')
+                logger.info('part %s done.' % page)
+            except Exception as e:
+                logger.warning('Exception:', exc_info=True)
+            
 
 
 if __name__ == '__main__':
-    nuclear = NuclearSpider(1273782, 1285939, 'http://1024.hlork9.pw/pw/htm_data/17/1809/')
+    nuclear = NuclearSpider(1262493, 1285939, 'http://1024.hlork9.pw/pw/htm_data/17/1809/')
     nuclear.start()
